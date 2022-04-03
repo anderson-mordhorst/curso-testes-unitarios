9ab29daefed875de756ec594a00c0b2036c6d968
anderson-mordhorst <anderson.mordhorst@gmail.com>
2022-04-03 19:44:00 -0300

    Commit inicial do projeto

---
+++ src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
@@ -0,0 +1,16 @@
+package br.ce.wcaquino.suites;
+
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+
+//@RunWith(Suite.class)
+@SuiteClasses({
+//	CalculadoraTest.class,
+	CalculoValorLocacaoTest.class,
+	LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+	//Remova se puder!
+}
